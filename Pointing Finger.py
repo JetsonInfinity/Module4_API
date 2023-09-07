7cd28896245de9c66bbcb7b5c1834b1d633e97c1
JetsonInfinity <144294007+JetsonInfinity@users.noreply.github.com>
2023-09-06 20:57:42 -0400

    Add files via upload

---
+++ Pointing Finger.py
@@ -0,0 +1,157 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 13 14:05:44 2023
+
+@author: micha
+"""
+
+########################################
+########################################
+#
+# A script that follows a finger across a screen using your computers webcam
+
+# CHALLENGE:
+# how would u scale the x and y movement to the position on the screen?
+
+
+# hint1: x and y coordinates on lines 112 and 113 correspond to position
+
+# hint2: do u need to pass more paramters through the movement function?
+
+########################################
+########################################
+########################################
+import cv2
+import mediapipe as mp
+import xarm
+import time
+import threading
+
+
+
+
+mp_drawing = mp.solutions.drawing_utils
+mp_hands = mp.solutions.hands
+arm = xarm.Controller('USB')
+servo4 = xarm.Servo(4)
+servo5 = xarm.Servo(5)
+servo6 = xarm.Servo(6)
+# For webcam input:
+cap = cv2.VideoCapture(0)
+
+with mp_hands.Hands(
+    min_detection_confidence=0.5,
+    min_tracking_confidence=0.5) as hands:
+  while cap.isOpened():
+    success, image = cap.read()
+    if not success:
+      print("Ignoring empty camera frame.")
+      continue
+
+    # Flip the image horizontally for a later selfie-view display, and convert
+    # the BGR image to RGB.
+    image = cv2.cvtColor(cv2.flip(image, 1), cv2.COLOR_BGR2RGB)
+    # To improve performance, optionally mark the image as not writeable to
+    # pass by reference.
+    image.flags.writeable = False
+    results = hands.process(image)
+
+    # Draw the hand annotations on the image.
+    image.flags.writeable = True
+    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+    if results.multi_hand_landmarks:
+      for hand_landmarks in results.multi_hand_landmarks:
+        mp_drawing.draw_landmarks(
+            image, hand_landmarks, mp_hands.HAND_CONNECTIONS)
+    
+        # Get coordinates of index finger
+        index_finger = hand_landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP]
+        x, y, z = int(index_finger.x * image.shape[1]), int(index_finger.y * image.shape[0]), int(index_finger.z * image.shape[1])
+        def movement(direction):
+          # position4 = arm.getPosition(servo4)
+          # position5 = arm.getPosition(servo5)
+          # position6 = arm.getPosition(servo6)
+          
+          if direction == "upleft":
+            arm.setPosition(4, 295,10, wait=True)
+            
+          
+            arm.setPosition(6, 900,10,wait=True)
+            
+              
+            #time.sleep(3)
+            return
+            
+            
+          elif direction == "downright":
+            
+            arm.setPosition(6, 0, 10,wait=True)
+            arm.setPosition(4, 700, 10, wait=True)
+            
+            #time.sleep(3)
+            return
+            
+            #arm.setPosition([[1,500],[2, 500],[3, 300],[4,500],[5,500],[6,165]])
+          elif direction =="upright":
+            arm.setPosition(4, 295, 10, wait=True)
+            arm.setPosition(6, 0, 10,wait=True)
+            
+           
+              
+            return
+            #time.sleep(3)
+            #arm.setPosition([[1,300],[2, 500],[3, 475],[4,410],[5,700],[6,500]])
+          elif direction =="downleft":
+            arm.setPosition(6, 900, 10,wait=True)
+           
+            arm.setPosition(4, 700, 10, wait=True)
+            
+            return
+            #arm.setPosition([[1,500],[2, 500],[3, 170],[4,900],[5,500],[6,500]])
+          
+        
+        
+        
+        
+        # Determine direction based on x and y coordinates
+        
+        
+        if y < image.shape[0]/2:
+            if x < image.shape[1]/2:
+              direction = "upleft"
+              movement(direction)
+            else: 
+              direction = "upright"
+              movement(direction)
+              
+              
+        else:
+            if x < image.shape[1]/2:
+              direction = "downleft"
+              
+              movement(direction)
+             
+            else:
+              direction = "downright"
+              
+              movement(direction)
+             
+        
+        print(direction)
+        
+        
+        
+       
+        
+        
+            
+
+            
+            
+
+    # Show the annotated image.
+    cv2.imshow('Hand Gestures', image)
+    arm.setPosition([[1,500],[2, 500],[3, 300],[4,500],[5,500],[6,165]])
+    if cv2.waitKey(5) & 0xFF == 27:
+      break
+cap.release()
---
+++ music_api.py
@@ -0,0 +1,142 @@
+#created on 2023-04-16 author @sabrina
+#MIT Open Source Initiative License 
+##############################################
+##############################################
+#
+##
+#Problem/Challenge:
+#Import songs from youtube and make the Robot dance to them
+#
+# requirements: 
+#               pip install youtube_search
+#               pip install python_vlc
+#               pip install pytube
+#               pip install tempocnn
+#               instalation of 64 bit (32 will not work) version of VLC https://get.videolan.org/vlc/3.0.11/win64/vlc-3.0.11-win64.exe
+#              
+#
+#
+#---------------importing modules-----------------------
+from __future__ import unicode_literals
+import youtube_dl
+import xarm #robotic arm module
+import time 
+import threading
+from youtube_search import YoutubeSearch
+import vlc
+from pytube import YouTube
+import librosa
+import numpy
+# import tempocnn
+# from tempocnn.classifier import TempoClassifier
+# from tempocnn.feature import read_features
+#------------initializing variables--------------------------
+arm = xarm.Controller('USB')
+TIME = 40 #seconds song will play
+
+#--------------functions------------------------------------
+
+    
+def downloadVideo(url, song_name):
+    while(1):
+        try:
+            YouTube(url).streams.get_audio_only().download()
+            break
+        except:
+            continue
+            
+
+    yt = YouTube(url)
+    while True:
+        try:
+            title = yt.title
+            break
+        except:
+            print("Failed to get name. Retrying...")
+            time.sleep(1)
+            yt = YouTube(url)
+            continue    
+    #title = yt.title
+    print(title)
+    try:
+        yt.filter(progressive=True, file_extension='mp4')
+        yt.order_by('resolution')[-1]
+        print("downloading")
+        yt.download()
+    except Exception as E:
+        print("already downloaded")
+    return (title+'.mp4')
+
+def dance(file):
+    global TIME
+    # # initialize the model (may be re-used for multiple files)
+    # classifier = TempoClassifier('model')
+
+    # # read the file's features
+    # features = read_features(file)
+
+    # # estimate the global tempo
+    # tempo = classifier.estimate_tempo(features, interpolate=False)
+    # # map beat to arm movements
+    for i in range(TIME):
+        if i % 2 == 0:
+            arm.setPosition(3,270,wait=False)
+            
+        else:
+            arm.setPosition(3,310,wait=False)
+            
+        #time.sleep(60 / tempo)
+        if i%3==0:
+            time.sleep(1)
+        
+def playAudio(file):
+    global TIME
+    # creating vlc media player object
+    media_player = vlc.MediaPlayer()
+    
+    dance_thread = threading.Thread(target=dance, args=([file]))
+    dance_thread.daemon = True 
+    
+    
+    
+    # media object
+    media = vlc.Media(file)
+    #start dance
+    dance_thread.start()
+    #time.sleep(10)
+    # setting media to the media player
+    media_player.set_media(media)
+    media_player.play()
+    
+    
+        
+       
+    
+    
+    time.sleep(TIME)#let song play for TIME seconds
+    
+   
+    
+    
+    
+ 
+
+        
+
+        
+    
+
+#--------------------main-------------------------------------
+
+
+
+    
+song_name = input("enter song name")
+results = YoutubeSearch(song_name, max_results=10).to_dict() #put results in a dictionary
+
+v = results.pop(0) #pop the first result from the result list
+url = ('https://www.youtube.com' + v['url_suffix'])
+
+file = downloadVideo(url, song_name)
+
+playAudio(file)
